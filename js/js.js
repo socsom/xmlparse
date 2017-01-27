2d6713d39194cdbc5038807ee377e248a3481192
socsom <socsom77@gmail.com>
2017-01-27 15:23:31 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,21 +1,14 @@
-var formElement=null;
-var secret=50;
- 
-//al cargar la página... 
-window.onload = function(){ 
- //pide los datos, lee preguntas.xml del servidor (por http)
- var xhttp = new XMLHttpRequest();
- xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-   gestionarXml(this);
-  }
- };
- xhttp.open("GET", "questions.xml", true);
- xhttp.send();
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+    }
+};
+xhttp.open("GET", "questions.xml", true);
+xhttp.send();
 
-
-//funcion donde cogemos los datos del xml y los ponemos en el html 
-function gestionarXml(dadesXml){
- var xmlDoc = dadesXml.responseXML;
- document.getElementById("title").innerHTML = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+    document.getElementById("title").innerHTML =
+    xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
 }
