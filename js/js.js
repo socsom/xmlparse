7c25c974efdeec4e97ae3d55f78d30085e11ad85
socsom <socsom77@gmail.com>
2017-02-16 13:41:07 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -6,54 +6,43 @@ xhttp.onreadystatechange = function() {
 };
 xhttp.open("GET", "questions.xml", true);
 xhttp.send();
-
-function myFunction(xml) {
-    var xmlDoc = xml.responseXML;
+//****************************************************************************************************
+// Recuperamos los datos del fichero XML xml/questions.xml
+// xmlDOC es el documento leido XML. 
+function gestionarXml(dadesXml) {
+    var xmlDoc = dadesXml.responseXML;//Parse XML to xmlDoc
+   
+  
+      //*****************************************************************************PREGUNTA 1*******************RADIO2
+ //Recuperamos el título y las opciones, guardamos las respuestas correctas
+ var title1 = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+ var opcionesRadio2 = [];
+ var nopt1 = xmlDoc.getElementById("pregunta_001").getElementsByTagName('option').length;
+ for (i = 0; i < nopt1; i++) { 
+    opcionesRadio2[i]=xmlDoc.getElementById("pregunta_001").getElementsByTagName('option')[i].childNodes[0].nodeValue;
+ }  
+ ponerDatosRadio2Html(title1,opcionesRadio2);
+ // FALTA GUARDAR LAS RESPUESTAS
+ 
+ // Ahora ponemos esos datos recuperados de titulos y opciones en el HTML con la siguiente funcion:
+function ponerDatosRadio2Html(t,opt){
+ var radioContainer=document.getElementById('title1');
+ var h3 = document.createElement("h3");
+ h3.innerHTML = t;
+ radioContainer.appendChild(h3); 
+ for (i = 0; i < opt.length; i++) { 
+    var input = document.createElement("input");
+    var label = document.createElement("label");
+    label.innerHTML=opt[i];
+    label.setAttribute("for", "color_"+i);
+    input.type="radio";
+    input.name="color";
+    input.id="color_"+i;;    
+    radioContainer.appendChild(input);
+    radioContainer.appendChild(label);
+ }  
+}       
     
-    document.getElementById("title").innerHTML =
-    xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
-    document.getElementById("r11").innerHTML =
-    xmlDoc.getElementsByTagName("option")[0].childNodes[0].nodeValue;
-    document.getElementById("r12").innerHTML =
-    xmlDoc.getElementsByTagName("option")[1].childNodes[0].nodeValue;
-    document.getElementById("r13").innerHTML =
-    xmlDoc.getElementsByTagName("option")[2].childNodes[0].nodeValue;
-    document.getElementById("r14").innerHTML =
-    xmlDoc.getElementsByTagName("option")[3].childNodes[0].nodeValue;
     
-    document.getElementById("title2").innerHTML =
-    xmlDoc.getElementsByTagName("title")[1].childNodes[0].nodeValue;
-    
-    document.getElementById("title3").innerHTML =
-    xmlDoc.getElementsByTagName("title")[2].childNodes[0].nodeValue;
-    document.getElementById("r31").innerHTML =
-    xmlDoc.getElementsByTagName("option")[4].childNodes[0].nodeValue;
-    document.getElementById("r32").innerHTML =
-    xmlDoc.getElementsByTagName("option")[5].childNodes[0].nodeValue;
-    document.getElementById("r33").innerHTML =
-    xmlDoc.getElementsByTagName("option")[6].childNodes[0].nodeValue;
-    
-    
-    document.getElementById("title4").innerHTML =
-    xmlDoc.getElementsByTagName("title")[3].childNodes[0].nodeValue;
-    document.getElementById("r41").innerHTML =
-    xmlDoc.getElementsByTagName("option")[7].childNodes[0].nodeValue;
-    document.getElementById("r42").innerHTML =
-    xmlDoc.getElementsByTagName("option")[8].childNodes[0].nodeValue;
-    document.getElementById("r43").innerHTML =
-    xmlDoc.getElementsByTagName("option")[9].childNodes[0].nodeValue;
-    
-    
-    document.getElementById("title5").innerHTML =
-    xmlDoc.getElementsByTagName("title")[4].childNodes[0].nodeValue;
-    document.getElementById("title6").innerHTML =
-    xmlDoc.getElementsByTagName("title")[5].childNodes[0].nodeValue;
-    document.getElementById("title7").innerHTML =
-    xmlDoc.getElementsByTagName("title")[6].childNodes[0].nodeValue;
-    document.getElementById("title8").innerHTML =
-    xmlDoc.getElementsByTagName("title")[7].childNodes[0].nodeValue;
-    document.getElementById("title9").innerHTML =
-    xmlDoc.getElementsByTagName("title")[8].childNodes[0].nodeValue;
-    document.getElementById("title10").innerHTML =
-    xmlDoc.getElementsByTagName("title")[9].childNodes[0].nodeValue;
+   
 }
